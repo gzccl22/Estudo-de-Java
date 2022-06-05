8c19538f37a0ec4893441bd4bcdf2d8fa4982605
Giovanna Zaccolon <102630771+gzccl22@users.noreply.github.com>
2022-06-04 23:22:57 -0300

    Add files via upload

---
+++ Introdução a Variávels/Introdução a Variável.md
@@ -0,0 +1,34 @@
+* # Introdução a Variável
+
+
+
+*variável é referencia na área de memoria que guarda algum valor*
+
+* Área de memória associada a um nome, que pode armazenar valores de um determinado tipo 
+
+##### Como declarar uma variável em java 
+
+* <tipo> <nome da variável>;
+* <tipo> <nome da variável> = <valor>;
+
+##### Convenção nomenclatura 
+
+<u>Palavras reservadas</u>
+
+abstract, continue , for,new,switch, assert, default, goto, package, synchronized, boolean, do ,if, private , this,break, double, implements, proteted, throw, byte, else, import, public,throws, case, enum, instanceof, return, transient,catch, extends, int, short, try, char, final, interface, static, void, class, finally, long, strictfp, volatile, const, float, native, super , while.
+
+----
+
+* a-z A-z _ &
+
+* a-z A-z _ & 0-9
+
+* CamelCase
+
+  
+
+  
+  
+  
+  
+  
---
+++ Introdução a Variávels/Variaveis.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author giova
+ */
+public class Variaveis {
+
+    public static void main(String[] args) {
+       //convenção Java
+        int idade =20;
+        String nome = "Giovanna";
+        String nomeDoMeuCachorro ="Max e Mel";
+        String ano2024;
+        
+        //aceito mas não utilizado
+        int _idade;
+        int $idade;
+        
+        //não é convenção Java 
+        String nome_do_meu_cachorro;
+        String NomeDoMeuCachorro;
+        String nomeDoMeucachorro;
+        
+        idade = 25;
+        
+      
+        
+        System.out.println("Idade="+idade);
+        System.out.println("Nome="+nome);
+    }
+}
